831247ad90cd26e3ed251eaf58fa37c5c9e5b59d
Nikhil <nikhil.verma@twinspark.co>
2019-12-20 13:18:43 +0530

    update color code

--- bc-promotions.php
+++ bc-promotions.php
@@ -91,7 +91,7 @@ function bc_promotion_shortcode( $atts , $content = null ) {
             $footer_heading = get_post_meta( get_the_ID(), 'promotion_footer_heading', true ); ?>
     
             <div class="col-md-4 col-lg-4 p-2 text-center">
-                <div class="bc_color_secondary bc_color_primary_bg p-3 mb-3">
+                <div class="bc_color_secondary bc_color_primary_bg p-3 mb-3" style="background-color: <?php echo $color;?>">
                     <div class="py-4 px-3 pt-0 border-white bc_coupon_container">
                         <span class="pb-3  bc_font_alt_1 bc_text_36 d-block"><?php echo $title; ?></span>
                         <span class="bc_text_30 d-block my-2"><?php echo $subheading;?></span>
